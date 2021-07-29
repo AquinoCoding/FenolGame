72f6964e7d46ab7e20878c8ccc6eec8d1c47d2cb
Lucas Aquino <lucasaqs09@gmail.com>
2021-07-29 14:27:51 -0300

    Sistema de Nivel

--- Program.cs
+++ Program.cs
@@ -14,20 +14,50 @@ namespace Game
             public string nome;
             public string sexo;
             public string tipo;
-            public string cep;
-            public string numCell;
-
         }
+
         static List<Personagem> personagens = new List<Personagem>();
-        static void Main(string[] args)
-        {
+        static Random lucky = new Random();
+        static bool conditionInitial = true;
+        static int NumFase = 0;
+
+        static void Main(string[] args){
+
             Console.WriteLine("Play to Game\n");
-            Console.WriteLine("Bem Vindo a Silion Valley\n");
+            Console.WriteLine("Bem Vindo a Silicon Valley\n");
+
+            AuthenticLevelUp();
             MenuPrincipal();
+            AuthenticLevelUp();
         }
 
-        static void MenuPrincipal()
-        {
+        static bool AuthenticLevelUp(){
+            
+
+            if (conditionInitial){
+
+                conditionInitial = false;
+                
+                LevelUp();
+            }
+            else{
+
+                LevelUp();
+            }
+            
+            return conditionInitial; 
+        }
+
+        static int LevelUp(){
+
+            NumFase = NumFase + 1;
+            Console.WriteLine($"Seu nivel: {NumFase}");
+            
+            return NumFase;
+
+        }
+
+        static void MenuPrincipal(){
 
             Console.WriteLine("--------Menu Principal--------\n");
 
@@ -35,14 +65,13 @@ namespace Game
 
             if (Option() == "1"){
                 MenuPersonagem();
-            }else
-            {
+            }else{
                 Console.WriteLine("Loandig...");
             }
 
         }
-        static void MenuPersonagem()
-        {
+
+        static void MenuPersonagem(){
 
             Personagem personagem = new Personagem();
 
@@ -68,13 +97,62 @@ namespace Game
 
             personagens.Add(personagem);
 
-            Console.WriteLine($"{personagem.nome} {personagem.sexo} {personagem.tipo}");
+            Console.WriteLine($"\n{personagem.nome} {personagem.sexo} {personagem.tipo} {Lucky()}");
         }
 
-        static string Option()
-        {
+        static string Option(){
             string option = Console.ReadLine();
             return option;
         }
+        
+        // Teste de Bonus, Só receberá a oportunidade de Bonus em fases de numero PAR
+        static string Lucky(){
+
+            // Condição para saber se a fase é PAR
+            int Fase = 1;
+            if (Fase % 2 == 0 || Fase == 1){
+
+                // Teste de Sorte
+
+                int dadosLucky = lucky.Next(10, 20);
+
+                //Console.WriteLine(dadosLucky);
+
+                if (dadosLucky > 10 || Fase == 1){
+
+                    Console.WriteLine("-----------BÔNUS----------\n");
+
+                    int dadosLuckyPremio = lucky.Next(1, 3);
+                    //Console.WriteLine(dadosLuckyPremio);
+
+                    if (dadosLuckyPremio == 1){
+                        string itemBonus = "Espada";
+                        Console.WriteLine($"A Coruja trouxe uma {itemBonus}");
+                        return "Espada";
+                    }
+                    else if (dadosLuckyPremio == 2){
+                        string itemBonus = "Escudo";
+                        Console.WriteLine($"A Coruja trouxe um {itemBonus}");
+                        return "Escudo";
+                    }
+                    else if (dadosLuckyPremio == 3){
+                        string itemBonus = "Moedas de ouro";
+                        int dadosLuckyMoedas = lucky.Next(1, 200);
+
+                        Console.WriteLine($"A Coruja trouxe $${dadosLuckyMoedas} {itemBonus}");
+                        return "Moedas de ouro";
+                    }
+
+                }else{
+                    
+                    return "A coruja extraviou sua entrega";
+
+                }
+                
+            }
+
+            return "";
+        }
+
     }
 }
